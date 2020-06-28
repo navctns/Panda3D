5eae00c698737f1ae5dfe4f5982388f63c3d33a1
NAVEEN <navsoft18@gmail.com>
2020-06-28 23:32:34 +0530

    Add existing file

--- gameview.py
+++ gameview.py
@@ -20,5 +20,14 @@ class MyGame(ShowBase):
     def __init__(self):
         super().__init__()
 
+        box = self.loader.loadModel('models/box')
+        box.setPos(0,10,0)
+        box.reparentTo(self.render)
+
+        panda = self.loader.loadModel('models/panda')
+        panda.setPos(-2,10,0)
+        panda.setScale(0.2,0.2,0.2)
+        panda.reparentTo(self.render)
+
 game = MyGame()
 game.run()
